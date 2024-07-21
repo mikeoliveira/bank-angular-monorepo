6f2bfa269d0459f6569cd2d6427eaa1fcfb0f18f
Mike Oliveira <mikeoliveira@outlook.com>
2024-07-21 18:28:06 -0300

    feat(libs-environments): create lib environments

---
+++ libs/shared/root/enviroments/src/lib/environments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-environments',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './environments.component.html',
+  styleUrl: './environments.component.css',
+})
+export class EnvironmentsComponent {}
